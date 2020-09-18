8b6ada0d645bed8f9984348da9acb27063496244
Yifan Wang <wyf0762@163.com>
2020-09-18 18:32:58 +0800

    Basic

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+# from you_get import common
+import os
+import zipfile
 
 app = Flask(__name__)
 
@@ -8,7 +11,24 @@ def index():
 
 @app.route("/", methods=['POST'])
 def download():
-    print(request.form.get("input"))
-    return render_template("index.html")
+    url = request.form.get("input")
+    cmd = f"you-get {url} -o tmp -O video"
+
+    print("Downloading ...")
+
+    # r = os.popen(cmd)  
+    # text = r.read()  
+    # r.close()  
+    # print("Got those: \n" + text)
+    
+    os.system(cmd)
+
+    # return render_template("index.html")
+
+    f = zipfile.ZipFile('test.zip', 'w', zipfile.ZIP_DEFLATED)
+    f.write("tmp/video.webm")
+    f.close()
+
+    return send_file("test.zip", as_attachment=True)
 
 app.run(debug=True)
